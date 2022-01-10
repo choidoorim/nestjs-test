e16cadd6685560ce121b60d80bdc5ec24a0ed154
choidoorim <doorim321@naver.com>
2022-01-10 23:37:41 +0900

    Update: ... Test

--- src/test/test.controller.ts
+++ src/test/test.controller.ts
@@ -11,4 +11,15 @@ export class TestController {
   async testFunc(@Param('testId') testId: number): Promise<string> {
     return this.testService.testService(testId);
   }
+
+  @Get()
+  async docsTest() {
+    return await this.testService.Docs({
+      summary: '휴대전화로 고객 확인 or 로그인',
+      description: '요약',
+      createdRes: {
+        description: '휴대전화로 고객 확인 or 로그인 성공',
+      },
+    });
+  }
 }
